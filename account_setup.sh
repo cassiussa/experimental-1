9c43c7f6164f65fff247668ab1b82eaa3b8d43f3
Cassius John-Adams <cassius.s.adams@gmail.com>
2018-11-28 13:33:32 -0500

    html-ify for a moment

--- account_setup.sh
+++ account_setup.sh
@@ -11,6 +11,9 @@ ENABLE_QA=${4}
 # Configure a Production environment for the Project?
 ENABLE_PROD=${5}
 
+
+MODE="html"
+
 FORMAT="Incorrect invocation of the 'account_setup' script.  :  account_setup.sh <customer_id> \"<name of the Project>\" <enable DEV? (options: true, false)> <enable QA? (options: true, false)> <enable PROD? (options: true, false)>"
 
 # ERROR CODES
@@ -41,28 +44,35 @@ PROJECT_PROD_GROUP="prod-group-${PROJECT_NAME}"
 PROJECT_QA_GROUP="qa-group-${PROJECT_NAME}"
 PROJECT_DEV_GROUP="dev-group-${PROJECT_NAME}"
 
-echo
-echo 
-echo "ACCOUNT-LEVEL USERS"
-echo "======================================================================================================"
-echo "ADMIN USER  : ${ADMIN_USER}  : An admin user with full permissions over all Projects you own"
-echo
-echo "ACCOUNT-LEVEL GROUPS"
-echo "======================================================================================================"
-echo "ADMIN GROUP : ${ADMIN_GROUP} : This group has admin permissions on all Projects you own"
-echo "PROD GROUP  : ${PROD_GROUP}  : If SysOps are responsible for Production environments in all Project, add them to this group"
-echo "QA GROUP    : ${QA_GROUP}    : If QA staff are responsible for QA environments in all Projects, add them to this group"
-echo "DEV GROUP   : ${DEV_GROUP}   : If developers are responsible for DEV environments in all Projects, add them to this group"
-echo 
-echo "PROJECT-LEVEL GROUPS"
-echo "======================================================================================================"
-echo "PROJECT ADMIN GROUP : ${PROJECT_ADMIN_GROUP} : This group has admin permissions of this Project only."
-echo "PROJECT PROD GROUP  : ${PROJECT_PROD_GROUP}  : If SysOps are reponsible for Production environments only at the Project-level, add them to this group."
-echo "PROJECT QA GROUP    : ${PROJECT_QA_GROUP}    : If QA staff are reponsible for QA environments only at the Project-level, add them to this group."
-echo "PROJECT DEV GROUP   : ${PROJECT_DEV_GROUP}   : If DEV staff are reponsible for DEV environments only at the Project-level, add them to this group."
-echo
-echo
-echo
+
+function outputMode() {
+  THIS_OUTPUT="${1}"
+  [[ "${MODE}" == "html" ]] && THIS_OUTPUT="echo \"<pre>${THIS_OUTPUT}</pre>\"" || THIS_OUTPUT="echo \"${THIS_OUTPUT}\""
+  eval "${THIS_OUTPUT}"
+}
+
+outputMode " "
+outputMode " "
+outputMode "ACCOUNT-LEVEL USERS"
+outputMode "======================================================================================================"
+outputMode "ADMIN USER  : ${ADMIN_USER}  : An admin user with full permissions over all Projects you own"
+outputMode " "
+outputMode "ACCOUNT-LEVEL GROUPS"
+outputMode "======================================================================================================"
+outputMode "ADMIN GROUP : ${ADMIN_GROUP} : This group has admin permissions on all Projects you own"
+outputMode "PROD GROUP  : ${PROD_GROUP}  : If SysOps are responsible for Production environments in all Project, add them to this group"
+outputMode "QA GROUP    : ${QA_GROUP}    : If QA staff are responsible for QA environments in all Projects, add them to this group"
+outputMode "DEV GROUP   : ${DEV_GROUP}   : If developers are responsible for DEV environments in all Projects, add them to this group"
+outputMode " "
+outputMode "PROJECT-LEVEL GROUPS"
+outputMode "======================================================================================================"
+outputMode "PROJECT ADMIN GROUP : ${PROJECT_ADMIN_GROUP} : This group has admin permissions of this Project only."
+outputMode "PROJECT PROD GROUP  : ${PROJECT_PROD_GROUP}  : If SysOps are reponsible for Production environments only at the Project-level, add them to this group."
+outputMode "PROJECT QA GROUP    : ${PROJECT_QA_GROUP}    : If QA staff are reponsible for QA environments only at the Project-level, add them to this group."
+outputMode "PROJECT DEV GROUP   : ${PROJECT_DEV_GROUP}   : If DEV staff are reponsible for DEV environments only at the Project-level, add them to this group."
+outputMode " "
+outputMode " "
+outputMode " "
 
 
 
@@ -71,7 +81,7 @@ unset DEPLOYMENT_ENVIRONMENT
 
 # ERROR OUT AND PROVIDE MESSAGE
 function errorExit () {
-    echo "Error: $1"
+    outputMode "Error: $1"
     exit 1
 }
 
@@ -95,7 +105,7 @@ fi
 
 # LOGIN TO CLUSTER
 function ocLogin() {
-  echo "oc login https://m.okd.supercass.com -u ${OKD_USERNAME} -p L4ma${OKD_PASSWORD}Zza --insecure-skip-tls-verify=true --config='/var/www/html/.kube/config'"
+  outputMode "oc login https://m.okd.supercass.com -u ${OKD_USERNAME} -p L4ma${OKD_PASSWORD}Zza --insecure-skip-tls-verify=true --config='/var/www/html/.kube/config'"
   LOGIN_COMMAND="oc login https://m.okd.supercass.com -u ${OKD_USERNAME} -p ${OKD_PASSWORD} --insecure-skip-tls-verify=true --config='/var/www/html/.kube/config'"
   # Error Code #394 - could not log into the OKD cluster using the oc command
   eval ${LOGIN_COMMAND} > /dev/null && return || errorExit "Unable to process request. Please contact support and provide Error Code #394."
@@ -106,13 +116,13 @@ function ocLogin() {
 # Accepts 3 parameters: ${1} Number of retries, ${2} time between retries, ${3} command to run
 function retryCommand() {
   # Iterate over the number of retries passed into the retryCommand function as 1st parameter
-  for retries in $(seq 1 $(echo "${1}")); do
+  for retries in $(seq 1 $(outputMode "${1}")); do
     # Log into the cluster
     ocLogin
     # Run the command ${3} parameter.  If it succeeds, return from function.  Otherwise echo failed and then retry ${1} number of times
     eval ${3} > /dev/null 2>&1 && return 
     #eval ${3} && return
-    [[ "${retries}" > 1 ]] && echo "Trying again in ${2} seconds"
+    [[ "${retries}" > 1 ]] && outputMode "Trying again in ${2} seconds"
     sleep ${2}
     # Exit out completely if we've failed to run the command ${1} times
     # Error Code #395 - Can't create Project, Project exists already, or cannot connect to the cluster via the 'oc' command.  The scipt's account may also have gotten logged out.
@@ -131,20 +141,20 @@ function createAdminUser() {
 
 
 function ensureAdminUserExist() {
-  #echo "function ensureAdminUserExist"
+  #outputMode "function ensureAdminUserExist"
   THIS_USER="${1}"
   POLL_FOR_USER="oc get user ${THIS_USER}"
-  #echo "See if admin  user exists.  If not, create it"
+  #outputMode "See if admin  user exists.  If not, create it"
   retryCommand "1" "3" "${POLL_FOR_USER}"
   POLL_FOR_USER_RESPONSE=$?
   if [[ "${POLL_FOR_USER_RESPONSE}" == 0 ]]; then
-    echo "The admin user '${THIS_USER}' already exists. Skipping..."
+    outputMode "The admin user '${THIS_USER}' already exists. Skipping..."
     return
   else 
-    echo "Creating new user: ${THIS_USER}"
+    outputMode "Creating new user: ${THIS_USER}"
     createAdminUser "${THIS_USER}" "some full name here"
     CREATE_ADMIN_USER_RESPONSE=$?
-    [[ ${CREATE_ADMIN_USER_RESPONSE} ]] && echo "Created admin user '${THIS_USER}'"; return || errorExit "Unable to create user ${THIS_USER}"
+    [[ ${CREATE_ADMIN_USER_RESPONSE} ]] && outputMode "Created admin user '${THIS_USER}'"; return || errorExit "Unable to create user ${THIS_USER}"
   fi
 }
 
@@ -159,7 +169,7 @@ function labelObject() {
   retryCommand "1" "3" "${COMMAND}"
   COMMAND_RESPONSE=$?
   if [[ "${COMMAND_RESPONSE}" == 0 ]]; then
-    echo "Labeled ${THIS_OBJECT_TYPE}"
+    outputMode "Labeled ${THIS_OBJECT_TYPE}"
   else
     errorExit "Error Code #393 - failed to label user."
   fi
@@ -219,20 +229,20 @@ function createAdminGroup() {
 
 
 function ensureAdminGroupExists() {
-  #echo "function ensureAdminGroupExists"
+  #outputMode "function ensureAdminGroupExists"
   THIS_GROUP="${1}"
   POLL_FOR_GROUP="oc get group ${THIS_GROUP}"
-  #echo "Verify the admin group exist.  If not, create it"
+  #outputMode "Verify the admin group exist.  If not, create it"
   retryCommand "1" "3" "${POLL_FOR_GROUP}"
   POLL_FOR_GROUP_RESPONSE=$?
   if [[ "${POLL_FOR_GROUP_RESPONSE}" == 0 ]]; then
-    echo "The admin group '${THIS_GROUP}' already exists. Skipping..."
+    outputMode "The admin group '${THIS_GROUP}' already exists. Skipping..."
     return
   else
-    echo "Creating new group: ${THIS_GROUP}"
+    outputMode "Creating new group: ${THIS_GROUP}"
     createAdminGroup "${THIS_GROUP}"
     CREATE_GROUP_RESPONSE=$?
-    [[ ${CREATE_GROUP_RESPONSE} ]] && echo "Created admin group '${THIS_GROUP}'"; return || errorExit "Unable to create group ${THIS_GROUP}"
+    [[ ${CREATE_GROUP_RESPONSE} ]] && outputMode "Created admin group '${THIS_GROUP}'"; return || errorExit "Unable to create group ${THIS_GROUP}"
   fi
 }
 
@@ -251,7 +261,7 @@ function createProject() {
 
 # CREATE NEW PROJECT
 function ensureProjectExists() {
-  #echo "function ensureProjectExists"
+  #outputMode "function ensureProjectExists"
   unset THIS_DEPLOYMENT_ENVIRONMENT
   unset THIS_DISPLAY_NAME
   unset THIS_DESCRIPTION
@@ -267,34 +277,34 @@ function ensureProjectExists() {
   fi
 
   POLL_FOR_PROJECT="oc get project ${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT,,}"
-  #echo "Verify the '${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT}' Project exist.  If not, create it"
+  #outputMode "Verify the '${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT}' Project exist.  If not, create it"
   retryCommand "1" "3" "${POLL_FOR_PROJECT}"
   POLL_FOR_PROJECT_RESPONSE=$?
   if [[ "${POLL_FOR_PROJECT_RESPONSE}" == 0 ]]; then
-    echo "The Project '${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT}' already exists.  Skipping..."
+    outputMode "The Project '${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT}' already exists.  Skipping..."
     return
   else
-    echo "Creating new Project: ${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT,,}"
-    #echo "createProject ${THIS_PROJECT_NAME} ${THIS_DEPLOYMENT_ENVIRONMENT,,} ${THIS_DISPLAY_NAME} ${THIS_DESCRIPTION}"
+    outputMode "Creating new Project: ${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT,,}"
+    #outputMode "createProject ${THIS_PROJECT_NAME} ${THIS_DEPLOYMENT_ENVIRONMENT,,} ${THIS_DISPLAY_NAME} ${THIS_DESCRIPTION}"
     createProject "${THIS_PROJECT_NAME}" "${THIS_DEPLOYMENT_ENVIRONMENT,,}" "${THIS_DISPLAY_NAME}" "${THIS_DESCRIPTION}"
     CREATE_PROJECT_RESPONSE=$?
-    [[ ${CREATE_PROJECT_RESPONSE} ]] && echo "Created Project '${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT,,}'"; return || errorExit "Unable to create the Project.  Please contact support and provide Error Code #395."
+    [[ ${CREATE_PROJECT_RESPONSE} ]] && outputMode "Created Project '${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT,,}'"; return || errorExit "Unable to create the Project.  Please contact support and provide Error Code #395."
   fi
 }
 
 
 function ensureAdminGroupPermissions() {
-  #echo "function ensureAdminGroupPermissions"
+  #outputMode "function ensureAdminGroupPermissions"
   unset COMMAND
   THIS_DEPLOYMENT_ENVIRONMENT="${1}"
   THIS_PROJECT_NAME="${2}"
   THIS_ADMIN_GROUP="${3}"
-  echo "Set permissions for Group: '${THIS_ADMIN_GROUP}'"
+  outputMode "Set permissions for Group: '${THIS_ADMIN_GROUP}'"
   COMMAND="oc adm policy add-role-to-group admin ${THIS_ADMIN_GROUP} -n ${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT}"
   retryCommand "1" "3" "${COMMAND}"
   COMMAND_RESPONSE=$?
   if [[ "${COMMAND_RESPONSE}" == 0 ]]; then
-    echo "Added administrative permissions to the '${THIS_ADMIN_GROUP}' Group on the '${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT}' Project"
+    outputMode "Added administrative permissions to the '${THIS_ADMIN_GROUP}' Group on the '${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT}' Project"
     return
   else
     errorExit "Failed to grant administrative permissions to the '${THIS_ADMIN_GROUP}' Group on the '${THIS_PROJECT_NAME}-${THIS_DEPLOYMENT_ENVIRONMENT}' Project"
@@ -304,18 +314,18 @@ function ensureAdminGroupPermissions() {
 
 
 function addUserToGroup() {
-  #echo "function addUserToGroup"
+  #outputMode "function addUserToGroup"
   unset COMMAND
   unset COMMAND_RESPONSE
   THIS_GROUP=${1}
   THIS_USER=${2}
-  echo "Adding the ${THIS_USER} user to the '${THIS_GROUP}' Group"
+  outputMode "Adding the ${THIS_USER} user to the '${THIS_GROUP}' Group"
   COMMAND="oc adm groups add-users ${THIS_GROUP} ${THIS_USER}"
-  echo "${COMMAND}"
+  outputMode "${COMMAND}"
   retryCommand "1" "3" "${COMMAND}"
   COMMAND_RESPONSE=$?
   if [[ "${COMMAND_RESPONSE}" == 0 ]]; then
-    echo "Added ${THIS_USER} to the '${THIS_GROUP}' Group"
+    outputMode "Added ${THIS_USER} to the '${THIS_GROUP}' Group"
     return
   else
     errorExit "Failed to add ${THIS_USER} to the '${THIS_GROUP}' Group"
@@ -398,9 +408,6 @@ if [[ "${ENABLE_PROD}" == true ]]; then
 fi
 
 
-debug "echo \"================================================\""
-debug "echo \"================================================\""
-
 
 # Add Users
 addUserToGroup "${ADMIN_GROUP}" "${ADMIN_USER}"
@@ -409,9 +416,9 @@ addUserToGroup "${PROJECT_ADMIN_GROUP}" "${ADMIN_USER}"
 
 
 
-echo "DONE DONE DONE"
+outputMode "DONE DONE DONE"
 
-exit 1
+#exit 1
 
 
 
